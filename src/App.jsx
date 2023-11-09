b70e18a9707b99dee74e8502c07d435de64b87f5
MiranaMiriyeva <arzumiri20@gmail.com>
2023-11-09 09:28:13 +0400

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React  from 'react'
+import Navbar from './components/Navbar'
+import Header from './components/Header'
+import Projects from './components/Projects'
+import About from './components/About'
+import Contact from './components/Contact'
+import Footer from './components/Footer'
+
+let App = () => {
+  
+  return (
+    <> 
+    <Navbar/>
+    <Header/>
+    <Projects/>
+    <About/>
+    <Contact/>
+    <Footer/>
+    </>
+  
+  )
+}
+
+export default App
